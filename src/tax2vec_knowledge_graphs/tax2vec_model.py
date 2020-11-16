2bdfa28ae33ad75082de4ba0b0ab19d1eb18189e
CitizenIsErased <timi.sp@gmail.com>
2020-11-16 19:19:47 +0100

    scores on the new split

--- src/tax2vec_knowledge_graphs/tax2vec_model.py
+++ src/tax2vec_knowledge_graphs/tax2vec_model.py
@@ -20,10 +20,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.decomposition import TruncatedSVD
 import pickle
-import tax2vec as t2v
 from preprocessing import *
+import tax2vec
 from tax2vec.preprocessing import *
 from tax2vec.models import *
+from scipy.sparse import coo_matrix, hstack
+from skopt import BayesSearchCV
+
 
 def get_features(data_train, data_validation):
     ## generate learning examples
@@ -31,7 +34,7 @@ def get_features(data_train, data_validation):
 
     ## do the stratified shufflesplit
     for _ in range(num_splits):
-        tax2vec_instance = t2v.tax2vec(
+        tax2vec_instance = tax2vec.tax2vec(
             max_features=50,
             targets=data_train['label'].to_list(),
             num_cpu=2,
@@ -59,43 +62,6 @@ def get_features(data_train, data_validation):
         features_test = hstack(test_matrices_for_svm)
 
     return features_train, features_test
-
-
-def train(data_train, data_validation):
-    ## generate learning examples
-    num_splits = 1
-
-    ## do the stratified shufflesplit
-    for _ in range(num_splits):
-        tax2vec_instance = t2v.tax2vec(
-            max_features=50,
-            targets=data_train['label'].to_list(),
-            num_cpu=2,
-            heuristic="closeness_centrality",
-            class_names=list(set(data_train['label'].to_list())))
-        semantic_features_train = tax2vec_instance.fit_transform(data_train['text_a'])
-        print(semantic_features_train)
-
-        ## get test features
-        train_matrices_for_svm = []
-        test_matrices_for_svm = []
-        semantic_features_test = tax2vec_instance.transform(data_validation['text_a'])
-
-        train_matrices_for_svm.append(semantic_features_train)
-        test_matrices_for_svm.append(semantic_features_test)
-
-        tfidf_word_train, tokenizer_2, _ = data_docs_to_matrix(
-            data_train['text_a'], mode="matrix_pan")
-        tfidf_word_test = tokenizer_2.transform(build_dataframe(data_validation['text_a']))
-        train_matrices_for_svm.append(tfidf_word_train)
-        test_matrices_for_svm.append(tfidf_word_test)
-
-        ## stack features (sparse)
-        features_train = hstack(train_matrices_for_svm)
-        features_test = hstack(test_matrices_for_svm)
-
-    return features_train, features_test
-
 
 def fit(X, y_train, cparam=50, max_iter = 5000):
     new_train_y = []
@@ -124,12 +90,21 @@ def evaluate(clf, X, test_y):
 
 if __name__ == "__main__":
     data_validation = parse_data.get_test()
-    data_train = parse_data.get_train()
+    data_train = parse_data.get_test()
 
     features_train, features_test = get_features(data_train, data_validation)
 
     pd.DataFrame(features_train.toarray()).to_csv("train_features.csv")
     pd.DataFrame(features_test.toarray()).to_csv("test_features.csv")
+    
+    #features_train = []
+    #features_test = []
+    
+    #s = pd.read_csv('features/train_features_kg.csv', sep=',')
+    #features_train = pd.DataFrame(s.to_numpy())
+
+    #s = pd.read_csv('features/test_features_kg.csv', sep=',')
+    #features_test = pd.DataFrame(s.to_numpy())
 
 
     model = fit(features_train, data_train['label'].to_list())
